f971dae3fd341322c6069b6012df24f4a10b3c11
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-09-17 22:58:11 +0530

    Add new updates and activities

--- Recap/StudentManagementSystem.java
+++ Recap/StudentManagementSystem.java
@@ -53,5 +53,12 @@ class Utils{
 				}
 				return true;
 			}
+			    public static String reverseString(String str) {
+				String reversed = "";
+				for (int i = str.length() - 1; i >= 0; i--) {
+					reversed += str.charAt(i);
+				}
+				return reversed;
+			}
 			
 	}
