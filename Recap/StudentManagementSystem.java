f1f29cb1cde3e306b1d8cec59af153fd294bb64f
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-09-17 23:10:18 +0530

    Add new updates and activities

--- Recap/StudentManagementSystem.java
+++ Recap/StudentManagementSystem.java
@@ -68,7 +68,7 @@ class Utils{
 					if (n == 1) return 0;
 					if (n == 2) return 1;
 					return fibonacci(n - 1) + fibonacci(n - 2);
-					} 
+					}	//me Fibonacci sequence ekak & mekath recurion ekak
 				
 			
 	}
