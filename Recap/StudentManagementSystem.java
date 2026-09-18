aae0df3077ca220eeabf2906f43e6a590cf2dbc2
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-09-18 22:51:42 +0530

    Add new updates and activities

--- Recap/StudentManagementSystem.java
+++ Recap/StudentManagementSystem.java
@@ -74,5 +74,5 @@ class Utils{
 //////////////MAIN CLASS///////////////////////	
 public class StudentManagementSystem{
 	static Scanner input = new Scanner (System.in);	//User gen input gannawa
-	static ArrayList<Student> students = new ArrayList<>();
+	static ArrayList<Student> students = new ArrayList<>();	//students walin data save karagannawa. array size eka wenas wenawane 
 	}
