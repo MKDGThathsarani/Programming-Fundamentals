3bb147c3f35806e6dd656d86e81f9769af0452c3
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-09-22 00:21:59 +0530

    Add new updates and activities

--- Recap/StudentManagementSystem.java
+++ Recap/StudentManagementSystem.java
@@ -117,5 +117,6 @@ public class StudentManagementSystem{
 			System.out.println("3. Search Student");
 			System.out.println("4. Sort Students");
 			System.out.println("5. Calculate Statistics");
+			System.out.println("6. Exit");
 			}
 	}
