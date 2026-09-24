7213271561495e7968dba53e80e5304bb4457fe1
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-09-24 23:17:27 +0530

    Add new updates and activities

--- Recap/Day_01/Demo.java
+++ Recap/Day_01/Demo.java
@@ -1,6 +1,18 @@
 public class Demo{
 	public static void main(String args[]){
+		//1.Variables 
 		int age = 22;
+		double salary = 50000.00;
+		String name = "Kamal";
+		boolean isStudent = true;
 		
+		//2.Print
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+		System.out.println("Salary: " + salary);
+		System.out.println("I Student: " + isStudent);
+		
+		//3.
+		System.out.println("Age Type: " + ((Object)age).getClass().getSimpleName());
 		}
 	}
